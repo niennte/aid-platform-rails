ff3bef75811bb5da877dfa0d4e61a7f0a895c719
Niennte <niennte@gmail.com>
2018-10-20 14:47:47 -0400

    Adjust SMTP settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,11 +35,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'https://peaceful-river-58348.herokuapp.com/',
+      domain:               'peaceful-river-58348.herokuapp.com',
       user_name:             ENV['GMAIL_USERNAME'],
       password:              ENV['GMAIL_PASSWORD'],
       authentication:       'plain',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,13 +60,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'https://peaceful-river-58348.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'peaceful-river-58348.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'https://peaceful-river-58348.herokuapp.com',
+      domain:               'peaceful-river-58348.herokuapp.com',
       user_name:             ENV['GMAIL_USERNAME'],
       password:              ENV['GMAIL_PASSWORD'],
       authentication:       'plain',
