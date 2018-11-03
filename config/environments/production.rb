1d7afb4b221c4c9b5cc0b23a0fceb0b43055a576
Niennte <niennte@gmail.com>
2018-11-03 17:39:51 -0400

    Instruct activeMailer to user Node app deployment in backlinks.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'peaceful-river-58348.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://infinite-sierra-74007.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
