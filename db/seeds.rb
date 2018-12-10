a356f788b584ed6e04b57ef2d4829127f65406f8
Niennte <niennte@gmail.com>
2018-12-09 20:21:48 -0500

    Clean up and improve seeds example script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ if system_user.nil?
   )
 end
 
-# create a 10000 users, with usernames and passwords as testn]@test.com and password[n]
+# create a 100 users, with usernames and passwords as user[N]@test.com and password[N]
 1.upto(100) do |i|
   User.create(
       username: "user#{i}",
@@ -35,38 +35,46 @@ end
   )
 end
 
-# create 5000 requests (distribute around GTA)
+# create 50 requests (distribute around GTA)
 center_point = { lat: 43.646791, lng: -79.526704 }
 1.upto(50) do |i|
-
+  user = User.find(rand(User.first.id..User.last.id))
   address = nil
-  while address.nil? do
-    address = Geocoder.search([center_point[:lat] + rand(-5.00..5.00), center_point[:lng] + rand(-5.00..5.00)]).first.address
-  end
-
-  Request.create!(
-      title: Faker::Lorem.sentence,
-      description: Faker::Lorem.paragraph(8),
-      address: address,
-      category: Request.categories.key(rand(0..1)), # get one from the category array
-      status: 'active', # get one from the status array
-      user: User.find(rand(User.first.id..User.last.id)),
+  Request.new(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph(8),
+    address: address,
+    category: Request.categories.key(rand(0..1)), # get random item from the category enum
+    status: 'active',
+    user: user,
   )
+  # this will reverse-geocode random set of coordinates
+  # ironically, some of them will not validate by geocoder -
+  # in which case do it over
+  until request.valid? do
+    address = Geocoder.search(
+      [
+        center_point[:lat] + rand(-5.00..5.00),
+        center_point[:lng] + rand(-5.00..5.00)
+      ]
+    ).first.address
+  end
+  request.save
 end
 
-# create 10000 responses, to random requests
+# create 100 responses, to random requests
 1.upto(100) do |i|
   request = Request.where(status: :active).order('id ASC').sample
   user = User.where.not(id: request.user_id).sample
 
   Response.create!(
-      message: 'Response from a volunteer',
-      request: request,
-      user: user
+    message: Faker::TvShows::DrWho.quote,
+    request: request,
+    user: user
   )
 end
 
-# fulfill requests, as posted by request posters
+# fulfill 15 requests, as posted by request posters
 1.upto(15) do |i|
 
   request = Request.select("requests.*, count(responses.id) as num_responses").joins("INNER JOIN responses ON requests.id = responses.request_id").where(status: :active).group("requests.id").sample
@@ -81,7 +89,7 @@ end
   fulfillment_poster.save
 end
 
-# fulfill 3000 requests, as posted by volunteers
+# fulfill 15 requests, as posted by volunteers
 1.upto(15) do |i|
   request = Request.select("requests.*, count(responses.id) as num_responses").joins("INNER JOIN responses ON requests.id = responses.request_id").where(status: :active).group("requests.id").sample
 
